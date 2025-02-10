e14512f7264728110188aa54a3c1865228fd55e9
Pepel <lera.kalashnikova2018@gmail.com>
2025-02-10 21:05:45 +0200

    done

---
+++ src/generics/4.ts
@@ -0,0 +1,21 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+function createOrUpdateUser(initialValues: Partial<User>): User {
+  const defaultUser: User = {
+    name: 'Unknown',
+    surname: 'Unknown',
+    email: 'Unknown',
+    password: 'Unknown',
+  };
+  return { ...defaultUser, ...initialValues };
+}
+
+createOrUpdateUser({
+  email: 'user@mail.com',
+  password: 'password123',
+});
